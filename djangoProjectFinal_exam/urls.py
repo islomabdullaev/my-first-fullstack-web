1e40f3aaee0ccdc63e1e5edfe91279674d7137bf
Islom <nolove02@bk.ru>
2021-10-09 04:16:52 -0400

    updated reqs

--- djangoProjectFinal_exam/urls.py
+++ djangoProjectFinal_exam/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path("", include("products.urls", namespace="products"))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
